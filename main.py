4c91fcca5962ff1a7fcc813ae529ba7b3388308b
ViLDiiK <vilim.ondrej@gmail.com>
2021-12-12 20:39:05 +0100

    done

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
 #!/usr/bin/env python3
 
 from os.path import basename, splitext
+from tkinter import Scale, HORIZONTAL, Canvas
 import tkinter as tk
 
-# from tkinter import ttk
-
-
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-        btn = tk.Button(self, text="Konec", command=self.close)
-        btn.pack()
-
-    def close(self):
-        self.destroy()
-
-
 class Application(tk.Tk):
-    name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = basename(splitext(basename(__file__))[0])
+    name = "ColorMishMash"
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
-        self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Hello World")
-        self.lbl.pack()
-        self.btn = tk.Button(self, text="Quit", command=self.quit)
-        self.btn.pack()
-        self.btn2 = tk.Button(self, text="About", command=self.about)
-        self.btn2.pack()
 
-    def about(self):
-        window = About(self)
-        window.grab_set()
+        self.bind("<Escape>", self.quit)
+
+        self.labelR = tk.Label(self, text="Red")
+        self.labelR.pack()
+        self.scaleR = Scale(self, from_=0, to=255, orient = HORIZONTAL, length= 256, command=self.change)
+        self.scaleR.pack()
+
+        self.labelG = tk.Label(self, text="Green")
+        self.labelG.pack()
+        self.scaleG = Scale(self, from_=0, to=255, orient = HORIZONTAL, length= 256, command=self.change)
+        self.scaleG.pack()
+
+        self.labelB = tk.Label(self, text="Blue")
+        self.labelB.pack()
+        self.scaleB = Scale(self, from_=0, to=255, orient = HORIZONTAL, length= 256, command=self.change)
+        self.scaleB.pack()
+
+        self.canvasMain = Canvas(self, width=256, height=128, background= "#000000")
+        self.canvasMain.pack()
+
+        self.buttonExit = tk.Button(self, text="Exit", command=self.quit)
+        self.buttonExit.pack()
+
+    def change(self, event):
+        r=self.scaleR.get()
+        g=self.scaleG.get()
+        b=self.scaleB.get()
+        self.canvasMain.config(background=f"#{r:02x}{g:02x}{b:02x}")
 
     def quit(self, event=None):
         super().quit()
-
 
 app = Application()
 app.mainloop()
