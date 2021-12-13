d0a00fabbfce8dc5118353688be44e131f809a24
ViLDiiK <vilim.ondrej@gmail.com>
2021-12-13 09:06:05 +0100

    done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from os.path import basename, splitext
-from tkinter import Scale, HORIZONTAL, Canvas
+from tkinter import Scale, HORIZONTAL, Canvas, LEFT, S, Frame, Entry
 import tkinter as tk
 
 class Application(tk.Tk):
@@ -14,20 +14,33 @@ class Application(tk.Tk):
 
         self.bind("<Escape>", self.quit)
 
-        self.labelR = tk.Label(self, text="Red")
-        self.labelR.pack()
-        self.scaleR = Scale(self, from_=0, to=255, orient = HORIZONTAL, length= 256, command=self.change)
-        self.scaleR.pack()
+        self.frameR = Frame(self)
+        self.frameR.pack()
+        self.frameG = Frame(self)
+        self.frameG.pack()
+        self.frameB = Frame(self)
+        self.frameB.pack()
 
-        self.labelG = tk.Label(self, text="Green")
-        self.labelG.pack()
-        self.scaleG = Scale(self, from_=0, to=255, orient = HORIZONTAL, length= 256, command=self.change)
-        self.scaleG.pack()
+        self.labelR = tk.Label(self.frameR, text="R")
+        self.labelR.pack(side=LEFT, anchor=S)
+        self.scaleR = Scale(self.frameR, from_=0, to=255, orient=HORIZONTAL, length= 256, command=self.change)
+        self.scaleR.pack(side=LEFT, anchor=S)
+        self.entryR = Entry(self.frameR, width=4)
+        self.entryR.pack(side=LEFT, anchor=S)
 
-        self.labelB = tk.Label(self, text="Blue")
-        self.labelB.pack()
-        self.scaleB = Scale(self, from_=0, to=255, orient = HORIZONTAL, length= 256, command=self.change)
-        self.scaleB.pack()
+        self.labelG = tk.Label(self.frameG, text="G")
+        self.labelG.pack(side=LEFT)
+        self.scaleG = Scale(self.frameG, from_=0, to=255, orient = HORIZONTAL, length= 256, command=self.change)
+        self.scaleG.pack(side=LEFT)
+        self.entryG = Entry(self.frameG, width=4)
+        self.entryG.pack(side=LEFT, anchor=S)
+
+        self.labelB = tk.Label(self.frameB, text="B")
+        self.labelB.pack(side=LEFT)
+        self.scaleB = Scale(self.frameB, from_=0, to=255, orient = HORIZONTAL, length= 256, command=self.change)
+        self.scaleB.pack(side=LEFT)
+        self.entryB = Entry(self.frameB, width=4)
+        self.entryB.pack(side=LEFT, anchor=S)
 
         self.canvasMain = Canvas(self, width=256, height=128, background= "#000000")
         self.canvasMain.pack()
