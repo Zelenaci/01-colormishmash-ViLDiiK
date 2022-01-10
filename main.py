0bddfd251335fb78ed3eeac3619b80f63c798ac8
ViLDiiK <vilim.ondrej@gmail.com>
2022-01-10 09:38:59 +0100

    set_scales pro clickhandler

--- main.py
+++ main.py
@@ -70,17 +70,34 @@ class Application(tk.Tk):
         self.canvasMem = []
         for row in range(3):
             for column in range(7):
-                canvas = Canvas(self.frameMem, width=50, height=50, background="#abcdef")
+                canvas = Canvas(self.frameMem, width=50, height=50, background="#ffffff")
                 canvas.bind("<Button-1>", self.clickHandler)
                 canvas.grid(row=row,column=column)
                 self.canvasMem.append(canvas)
 
+        
         self.load()
 
+    def set_scales(self):
+        color = self.canvasMain.cget("background")
+        r=int(color[1:3],16)
+        g=int(color[3:5],16)
+        b=int(color[5:],16)
+        self.varR.set(r)
+        self.varG.set(g)
+        self.varB.set(b)
+
     def load(self):
-        with open("paleta.txt", "r") as f:
-            colorMain = f.readline().strip()
-            self.canvasMain.config(background=colorMain)
+        try:
+            with open("paleta.txt", "r") as f:
+                colorMain = f.readline().strip()
+                self.canvasMain.config(background=colorMain)
+                self.set_scales()
+                for canvas in self.canvasMem:
+                    colorMem = f.readline().strip()
+                    canvas.config(background=colorMem)
+        except FileNotFoundError:
+            print("Vyskytla se chyba při načítání konfiguračního souboru.")
 
     def change(self, var=None, index=None, mode=None):
         r = self.scaleR.get()
